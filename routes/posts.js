a87a07262b22dae7329257c9a18b657eed2c8c55
Luca Giampieri <luca.giampieri95@gmail.com>
2026-02-10 09:42:32 +0100

    aggiunta cartella publi con foto

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const postsList = require('../data/posts');
-
 const postController = require('../controllers/postController');
 
 //rotte du CRUD
