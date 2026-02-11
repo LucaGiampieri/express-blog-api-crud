c6a6618797771ed166840860611cbb416b8f8dbc
Luca Giampieri <luca.giampieri95@gmail.com>
2026-02-11 15:20:53 +0100

    Aggiunta middlewares update, modify e destroy Request

--- routes/posts.js
+++ routes/posts.js
@@ -6,6 +6,10 @@ const postController = require('../controllers/postController');
 const indexRequest = require('../middlewares/request/indexRequest');
 const showRequest = require('../middlewares/request/showRequest');
 const storeRequest = require('../middlewares/request/storeRequest');
+const modifyRequest = require('../middlewares/request/modifyRequest');
+const updateRequest = require('../middlewares/request/updateRequest');
+const destroyRequest = require('../middlewares/request/destroyRequest');
+
 
 //rotte du CRUD
 
@@ -19,12 +23,12 @@ router.get('/:id', showRequest, postController.show);
 router.post('/', storeRequest, postController.store);
 
 // update
-router.put('/:id', postController.update);
+router.put('/:id', updateRequest, postController.update);
 
 // modify
-router.patch('/:id', postController.modify);
+router.patch('/:id', modifyRequest, postController.modify);
 
 // destroy
-router.delete('/:id', postController.destroy);
+router.delete('/:id', destroyRequest, postController.destroy);
 
 module.exports = router;
